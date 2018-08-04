27d02dcd187d3b319cbd682834bb598684966705
Ozawa Masayuki <ozawa@coroq.com>
2018-08-04 14:53:46 +0900

    Add tests for Form

---
+++ test/FormTest.php
@@ -0,0 +1,59 @@
+<?php
+use Coroq\Form;
+use Coroq\Input;
+
+class FormTest extends PHPUnit_Framework_TestCase {
+  public function testGetItemIn() {
+    $form = new Form();
+    $a = (new Input())->setValue("a");
+    $form->setItem("a", $a);
+    $c = (new Input())->setValue("c");
+    $b = (new Form())->setItem("c", $c);
+    $form->setItem("b", $b);
+    $this->assertSame($a, $form->getItemIn("a"));
+    $this->assertSame($b, $form->getItemIn("b"));
+    $this->assertSame($c, $form->getItemIn("b/c"));
+  }
+
+  /**
+   * @expectedException \LogicException
+   */
+  public function testGetItemThrowsExceptionIfNoItem() {
+    $form = new Form();
+    $form->getItem("a");
+  }
+  
+  public function testGetValueCollectsValuesOnlyFromEnabledItems() {
+    $form = new Form();
+    $form->setItem("a", (new Input())->setValue("a"));
+    $form->setItem("b", (new Input())->setValue("b")->disable());
+    $this->assertEquals(["a" => "a"], $form->getValue());
+  }
+  
+  public function testSetValue() {
+    $form = new Form();
+    $form->setItem("a", (new Input())->setValue("a"));
+    $form->setItem("b", (new Input())->setValue("b"));
+    $form->setItem("c", new Form());
+    $form->getItem("c")->setItem("d", (new Input())->setValue("d"));
+    $form->getItem("c")->setItem("e", (new Input())->setValue("e"));
+    $form->setValue([
+      "a" => "A",
+      "c" => [
+        "d" => "D",
+      ],
+      "f" => "F",
+      "g" => [
+        "h" => "H",
+      ],
+    ]);
+    $this->assertEquals([
+      "a" => "A",
+      "b" => null,
+      "c" => [
+        "d" => "D",
+        "e" => null,
+      ],
+    ], $form->getValue());
+  }
+}
