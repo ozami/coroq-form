0f24589caea5a08c979cbff84a4818c15d81115c
Ozawa Masayuki <ozawa@coroq.com>
2020-10-17 09:57:34 +0900

    Lowercase email address by default

--- src/Input/Email.php
+++ src/Input/Email.php
@@ -2,9 +2,35 @@
 namespace Coroq\Input;
 
 class Email extends Text {
+  private $lowercase_domain;
+
   public function __construct() {
     parent::__construct();
     $this->setMb("as");
+    $this->setLowerCaseDomain(true);
+  }
+
+  /**
+   * @param bool $lowercase_domain
+   * @return $this
+   */
+  public function setLowerCaseDomain($lowercase_domain = true) {
+    $this->lowercase_domain = $lowercase_domain;
+    return $this;
+  }
+
+  /**
+   * @param mixed $value
+   * @return string
+   */
+  public function filter($value) {
+    $value = parent::filter($value);
+    if ($this->lowercase_domain) {
+      $value = preg_replace_callback('#(.*@)(.*)#', function(array $matches) {
+        return $matches[1] . strtolower($matches[2]);
+      }, $value);
+    }
+    return $value;
   }
 
   /**
--- test/Input/EmailTest.php
+++ test/Input/EmailTest.php
@@ -6,7 +6,24 @@ class EmailTest extends PHPUnit_Framework_TestCase {
     $input = (new Email())->setValue(" ＴＥＳＴ＠example.com ");
     $this->assertSame("TEST@example.com", $input->getValue());
   }
-  
+
+  public function testLowerCaseDomain() {
+    $input = new Email();
+    $input->setValue("TEST@EXAMPLE.COM");
+    $this->assertSame("TEST@example.com", $input->getValue());
+    $input->setValue("TEST-EXAMPLE.COM");
+    $this->assertSame("TEST-EXAMPLE.COM", $input->getValue());
+    $input->setValue("TEST@TEST@EXAMPLE.COM");
+    $this->assertSame("TEST@TEST@example.com", $input->getValue());
+  }
+
+  public function testLowerCaseDomainDisabled() {
+    $input = new Email();
+    $input->setLowercaseDomain(false);
+    $input->setValue("TEST@EXAMPLE.COM");
+    $this->assertSame("TEST@EXAMPLE.COM", $input->getValue());
+  }
+
   public function testValidate() {
     $input = new Email();
     $input->setValue("valid@example.com")->validate();
